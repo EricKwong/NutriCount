525d13b8d8ad678e1b912f58a20f09acc3cf8947
Brice Lin <bricejlin@gmail.com>
2015-07-11 23:47:00 -0400

    Hook up to backend for data

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,19 +1,22 @@
 var request = require('superagent');
 var config = require('../../conf.json');
 
+var diets = require('../data/diets.json');
+var cps = require('cps-api');
+
 angular.module('starter.controllers', [])
 
-.controller('LoginCtrl', function($scope, $state, Username, UserDb) {
+.controller('LoginCtrl', function($scope, $state, User) {
   $scope.username = '';
   $scope.checkUser = function(username) {
     request
       .post('https://api-us.clusterpoint.com/100785/NutriCount-Users/_search.json')
-      .send({ query: '<username>' + username + '</username>' })
+      .send({ query: cps.Term(username, 'username') })
       .set('Authorization', 'Basic ' + btoa(config.user + ':' + config.password))
       .end(function (err, res) {
         if (res.ok) {
-          Username.setUsername(username);
           if (res.body.documents) {
+            User.setInfo(res.body.documents[0]);
             $state.go('tab.status');
           } else {
             $state.go('diet');
@@ -23,11 +26,8 @@ angular.module('starter.controllers', [])
   };
 })
 
-.controller('DietCtrl', function($scope, $state, username) {
-  $scope.userData = {
-    username: username,
-    dietId: null
-  };
+.controller('DietCtrl', function($scope, $state, User) {
+  $scope.userData = User.getInfo();
 
   $scope.goToStatus = function() {
     $state.go('tab.status');
@@ -40,6 +40,35 @@ angular.module('starter.controllers', [])
     // navigate to stats page with template
     $scope.goToStatus();
   };
+})
+
+.controller('StatusCtrl', function($scope, $state, User) {
+  $scope.userData = User.getInfo();
+
+  if (!$scope.userData) {
+    $state.go('login');
+  }
+  $scope.userNutrition;
+  $scope.template = diets[$scope.userData.dietId].nutrition;
+
+  var aggregation = 'sum(calories) as calories, ' +
+                    'sum(sugars) as sugars, ' +
+                    'sum(protein) as protein, ' +
+                    'sum(totalFat) as totalFat';
+
+  // Make agg search
+  request
+    .post('https://api-us.clusterpoint.com/100785/NutriCount-FoodLog/_search.json')
+    .send({ query: cps.Term($scope.userData.id, 'userId'), aggregate: aggregation })
+    .set('Authorization', 'Basic ' + btoa(config.user + ':' + config.password))
+    .end(function (err, res) {
+      if (res.ok) {
+        if (res.body.aggregate) {
+          $scope.userNutrition = res.body.aggregate[0].data[0];
+          $scope.$apply();
+        }
+      }
+    });
 })
 
 .controller('CameraCtrl', function($scope, Camera) {
